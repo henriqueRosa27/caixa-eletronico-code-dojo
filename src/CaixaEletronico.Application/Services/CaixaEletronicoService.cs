057b2085e4766b4d3dc4c9634fe6bb59c29fd85b
Henrique Rosa <31964245+henriqueRosa27@users.noreply.github.com>
2023-08-02 13:22:34 -0300

    aaa

---
+++ src/CaixaEletronico.Application/Services/CaixaEletronicoService.cs
@@ -0,0 +1,12 @@
+﻿namespace CaixaEletronico.Application.Services
+{
+    public class CaixaEletronicoService
+    {
+        private readonly IEnumerable<int> NotasDisponiveis = new List<int> { 100, 50, 20, 10, 5, 2 };
+
+        public IEnumerable<int> CalculaSaque(int valor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ testes/CaixaEletronico.Testes/Services/CaixaEletronicoServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using CaixaEletronico.Application.Services;
+
+namespace CaixaEletronico.Testes.Services
+{
+    public class CaixaEletronicoServiceTest
+    {
+        private readonly CaixaEletronicoService _service;
+
+        public CaixaEletronicoServiceTest()
+        {
+            _service = new CaixaEletronicoService();
+        }
+
+        [Fact]
+        public void CalculaSaquePara30()
+        {
+            var notas = _service.CalculaSaque(30);
+
+            // ambos são iguais
+            //Assert.Equal(1, notas.Where(n => n == 20).Count());
+            Assert.Single(notas.Where(n => n == 20));
+
+            Assert.Single(notas.Where(n => n == 10));
+        }
+    }
+}
